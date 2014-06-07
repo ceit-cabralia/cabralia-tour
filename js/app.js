40c0ac018a4926537250254ab795bc0912744feb
Vitor Leal <vitorleal1@gmail.com>
2014-06-07 15:26:25 -0300

    adicionando script para compilar templates

--- js/app.js
+++ js/app.js
@@ -1,47 +1,50 @@
+//Url padrão da nossa api
+var apiUrl = 'http://cabralia.herokuapp.com/api/';
+
+//Funções da aplicação
+var app = {
+  init: function () {
+    //iniciamos o loader
+    helpers.loader.show();
+
+    //Iniciamos o request para a nossa api
+    qwest.get(apiUrl + 'categoria')
+      .success(function (response) {
+        helpers.getTemplate('menu-list-template', { categoria: response }, 'menu-list');
+      })
+      .error(function (message) {
+        console.log(message);
+      })
+      .complete(function (message) {
+        //escondemos o loader
+        helpers.loader.hide();
+      });
+  }
+};
+
+//Funções auxiliares
+var helpers = {
+  getTemplate: function (template, data, destination) {
+     var source = document.getElementById(template).innerHTML,
+          temp  = Handlebars.compile(source);
+
+     var content = temp(data),
+         dest    = document.getElementById(destination);
+
+     dest.innerHTML = content;
+  },
+  loader: {
+    show: function () {
+      var loader = document.getElementById('loader');
+      loader.classList.remove('hide');
+    },
+    hide: function () {
+      var loader = document.getElementById('loader');
+      loader.classList.add('hide');
+    }
+  }
+};
 
 
-// chamada da opção Hospedagens
-document.querySelector('#btn-hospedagem').addEventListener('click', function () {
-    document.querySelector('#list-hospedagem').className = 'current';
-    document.querySelector('[data-position="current"]').className = 'left';
-});
-
-document.querySelector('#btn-buttons-back').addEventListener('click', function () {
-    document.querySelector('#list-hospedagem').className = 'right';
-    document.querySelector('[data-position="current"]').className = 'current';
-});
-
-// chamada da opção restaurantes
-document.querySelector('#btn-restaurante').addEventListener('click', function () {
-    document.querySelector('#list-restaurantes').className = 'current';
-    document.querySelector('[data-position="current"]').className = 'left';
-});
-
-document.querySelector('#btn-buttons-back1').addEventListener('click', function () {
-    document.querySelector('#list-restaurantes').className = 'right';
-    document.querySelector('[data-position="current"]').className = 'current';
-});
-
-// chamada da opção pontos turisticos
-document.querySelector('#btn-pontostour').addEventListener('click', function () {
-    document.querySelector('#list-pontosturisticos').className = 'current';
-    document.querySelector('[data-position="current"]').className = 'left';
-});
-
-document.querySelector('#btn-buttons-back2').addEventListener('click', function () {
-    document.querySelector('#list-pontosturisticos').className = 'right';
-    document.querySelector('[data-position="current"]').className = 'current';
-});
-
-// chamada da opção Transportes
-document.querySelector('#btn-transporte').addEventListener('click', function () {
-    document.querySelector('#list-transportes').className = 'current';
-    document.querySelector('[data-position="current"]').className = 'left';
-});
-
-document.querySelector('#btn-buttons-back3').addEventListener('click', function () {
-    document.querySelector('#list-transportes').className = 'right';
-    document.querySelector('[data-position="current"]').className = 'current';
-});
-
-
+//Iniciamos o app
+app.init();
