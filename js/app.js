765c88d4f4cac9c707f49d899376fbb8429c0fc7
Vitor Leal <vitorleal1@gmail.com>
2014-06-07 19:11:50 -0300

    add click in the menu item

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ var app = {
         helpers.getTemplate('menu-list-template', { categoria: response }, 'menu-list');
       })
       .error(function (message) {
-        console.log(message);
+        //tratamos o erro
       })
       .complete(function (message) {
         //escondemos o loader
@@ -25,13 +25,62 @@ var app = {
 //Funções auxiliares
 var helpers = {
   getTemplate: function (template, data, destination) {
-     var source = document.getElementById(template).innerHTML,
-          temp  = Handlebars.compile(source);
+    var source = document.getElementById(template).innerHTML,
+         temp  = Handlebars.compile(source);
 
-     var content = temp(data),
-         dest    = document.getElementById(destination);
+    var content = temp(data),
+        dest    = document.getElementById(destination);
 
-     dest.innerHTML = content;
+    dest.innerHTML = content;
+
+    //Adicionamos click nos itens do menu
+    var menuLinks = document.querySelectorAll('.menu-item');
+    for (var i = 0, l = menuLinks.length; i < l; i ++) {
+      var item = menuLinks[i];
+      item.addEventListener('click', helpers.goTo);
+    }
+
+    //Adicionamos click nos botões voltar
+    var back = document.querySelectorAll('.go-back');
+    for (var i = 0, l = back.length; i < l; i ++) {
+      var item = back[i];
+      item.addEventListener('click', helpers.goBack);
+    }
+
+  },
+  goTo: function (e) {
+    var categoria = this.getAttribute('data-categoria'),
+        title     = this.getAttribute('data-title');
+
+    //Inserimos o novo titulo na pagina de listas
+    helpers.setTitle(title);
+
+    //Mostramos o loader
+    helpers.loader.show();
+
+    //Fazemos o get da url de itens passando a categoria
+    qwest.get(apiUrl + 'item/categoria/' + categoria)
+      .success(function (response) {
+        helpers.getTemplate('item-list-template', { itens: response }, 'item-list');
+
+        document.getElementById('list-item-page').className = 'current';
+        document.querySelector('[data-position="current"]').className = 'left';
+      })
+      .error(function (message) {
+        //tratamos o erro
+      })
+      .complete(function (message) {
+        //escondemos o loader
+        helpers.loader.hide();
+      });
+  },
+  goBack: function () {
+    document.getElementById('list-item-page').className = 'right';
+    document.querySelector('[data-position="current"]').className = 'current';
+  },
+  setTitle: function (title) {
+    var listTitle = document.getElementById('item-list-title');
+    listTitle.textContent = title;
   },
   loader: {
     show: function () {
