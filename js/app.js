dcbd5429a0aa3555d2bc8975ddb919850c0029b1
Claércio Santos <claerciomg@gmail.com>
2014-07-25 04:17:52 -0300

    Ajustes Gerais
    Eliminação de imagens não utilizadas, correções em telas de detalhes,
    chamada da API do discador

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//Url padrão da nossa api
+///Url padrão da nossa api
 
 var apiUrl ='http://hayoapi.herokuapp.com/';
 //Funções da aplicação
@@ -44,19 +44,32 @@ var helpers = {
       item.addEventListener('click', helpers.goTo);
     }
 
-    //Adicionamos click nos itens do menu
+    //Adicionamos click nos itens das telas de listagem
     var menuLinks = document.querySelectorAll('.item-list');
     for (var i = 0, l = menuLinks.length; i < l; i++) {
       var item = menuLinks[i];
       item.addEventListener('click', helpers.goDetalhe);
     }
   
-
-    //Adicionamos click nos botões voltar
+    //Adicionamos click nos botões voltar das telas de listagem
     var back = document.querySelectorAll('.go-back');
     for (var i = 0, l = back.length; i < l; i++) {
       var item = back[i];
       item.addEventListener('click', helpers.goBack);
+    }
+
+    //Adicionamos click nos botões voltar das telas de detalhes
+    var back = document.querySelectorAll('.go-back_D');
+    for (var i = 0, l = back.length; i < l; i++) {
+      var item = back[i];
+      item.addEventListener('click', helpers.goBack_D);
+    }
+
+    //Adicionamos click nos botões voltar da tela Sobre Cabrália
+    var back = document.querySelectorAll('.go-back_S');
+    for (var i = 0, l = back.length; i < l; i++) {
+      var item = back[i];
+      item.addEventListener('click', helpers.goBack_S);
     }
 
   },
@@ -67,22 +80,30 @@ var helpers = {
     title = this.getAttribute('data-title');
 
     //Inserimos o novo titulo na pagina de listas
-    // chama a tela de listagem, fazendo a transição 
-    document.getElementById('list-item-page').className = 'current';
-    document.querySelector('[data-position="current"]').className = 'left';
-
+    
     helpers.setTitle(title);
+  
+    
+    if (categoria == 'sobre') {
 
-    //Mostramos o loader
-    helpers.loader.show();
+      // chama a tela Sobre Cabrália
+      document.getElementById('sobre-cabralia-page').className = 'current';
+      document.querySelector('[data-position="current"]').className = 'left'; 
+    
+    } else {
 
-    //Fazemos o get da url de itens passando a categoria
-    qwest.get(apiUrl + categoria)
+      //Mostramos o loader
+      helpers.loader.show();
+ 
+      qwest.get(apiUrl + categoria)
       .success(function (response) {
         helpers.getTemplate('item-list-template', {
           itens: response
         }, 'item-list');
 
+      // chama a tela de listagem, fazendo a transição 
+      document.getElementById('list-item-page').className = 'current';
+      document.querySelector('[data-position="current"]').className = 'left';    
         
       })
       .error(function (message) {
@@ -91,7 +112,9 @@ var helpers = {
       .complete(function (message) {
         //escondemos o loader
         helpers.loader.hide();
-      });
+    });
+  }
+ 
   },
 
   goDetalhe: function (e) {
@@ -101,43 +124,75 @@ var helpers = {
     var colection = this.getAttribute('data-colection');
     var id = this.getAttribute('data-id');
     
-
-
     //Inserimos o novo titulo na pagina de listas
-    // chama a tela de listagem, fazendo a transição 
-    document.getElementById('detalhe-item-page').className = 'current';
-    document.querySelector('[data-position="current"]').className = 'left';
-
     helpers.setTitle(title);
 
     //Mostramos o loader
     helpers.loader.show();
 
-    //Fazemos o get da url de itens passando a categoria
-    qwest.get(apiUrl + colection)
-      .success(function (response) {
+    //Fazendo o get da url de itens passando a categoria
+    qwest.get(apiUrl + colection + id)
+      .success(function (response) {  
+
+         // converte o objeto JSON e strint
+         var lixo = JSON.stringify(response);
+         // acrescenta '[' ']' na string para leitura corrreta no helpers
+         lixo='['+lixo+']';
+         // converte a string modificada em objegto JSON novamente 
+         response = JSON.parse(lixo);      
+
          helpers.getTemplate('detalhe-tela-template', {
           detalhe:  response 
-        }, 'detalhe-list');
+        }, 'detalhe-list');  
 
-        
+        // chama a tela de listagem, fazendo a transição 
+        document.getElementById('detalhe-item-page').className = 'current';
+        document.querySelector('[data-position="current"]').className = 'left'; 
       })
       .error(function (message) {
         //tratamos o erro
       })
       .complete(function (message) {
-        //escondemos o loader
-        helpers.loader.hide();
+         //escondemos o loader
+         helpers.loader.hide();
+
+         document.querySelector('#telef').addEventListener('click', helpers.goCall);
       });
+
+     
   },
 
   // chama a tela de Detalhe do ITEM selecionado
-  
-
   goBack: function () {
     document.getElementById('list-item-page').className = 'right';
     document.querySelector('[data-position="current"]').className = 'current';
   },
+
+  // chama a tela de Detalhe do ITEM selecionado
+  goBack_D: function () {
+    document.getElementById('detalhe-item-page').className = 'right';
+    document.querySelector('[data-position="current"]').className = 'current';
+  },
+
+  goBack_S: function () {
+    document.getElementById('sobre-cabralia-page').className = 'right';
+    document.querySelector('[data-position="current"]').className = 'current';
+  },
+
+  goCall: function () {
+    //alert('Pegou o click do telefone');
+
+    var numero = this.getAttribute('data-tel');
+    
+    var call = new MozActivity({
+    name: "dial",
+    data: {
+        number: numero
+    }
+    });
+
+  },
+
   setTitle: function (title) {
     var listTitle = document.getElementById('item-list-title');
     listTitle.textContent = title;
@@ -155,3 +210,7 @@ var helpers = {
 };
 
 app.init();
+
+addEventListener(document.querySelector('telef'), 'click', function () {
+    alert('Testando o clique do telefone');
+});
