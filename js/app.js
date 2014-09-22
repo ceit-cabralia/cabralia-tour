79aa5d473c6d19d08302c576e31173931171815b
Claércio Santos <Claercio@users.noreply.github.com>
2014-09-22 14:49:21 -0300

    Indentação de código
    Indentação de códigos JavaScript

--- js/app.js
+++ js/app.js
@@ -1,14 +1,10 @@
-///Url padrão da nossa api
-
 var apiUrl ='localhost/3000';
-//Funções da aplicação
+
 var app = {
   init: function () {
         
-    //iniciamos o loader
     helpers.loader.show();
 
-    //Iniciamos o request para a nossa api
     qwest.get(apiUrl)
       .success(function (response) {
         helpers.getTemplate('menu-list-template', {
@@ -16,16 +12,15 @@ var app = {
         }, 'menu-list');
       })
       .error(function (message) {
-        //tratamos o erro
       })
       .complete(function (message) {
-        //escondemos o loader
+
         helpers.loader.hide();
       });
   }
 };
 
-//Funções auxiliares
+
 var helpers = {
   getTemplate: function (template, data, destination) {
     var source = document.getElementById(template).innerHTML,
@@ -37,35 +32,35 @@ var helpers = {
 
     dest.innerHTML = content;
 
-    //Adicionamos click nos itens do menu
+
     var menuLinks = document.querySelectorAll('.menu-item');
     for (var i = 0, l = menuLinks.length; i < l; i++) {
       var item = menuLinks[i];
       item.addEventListener('click', helpers.goTo);
     }
 
-    //Adicionamos click nos itens das telas de listagem
+
     var menuLinks = document.querySelectorAll('.item-list');
     for (var i = 0, l = menuLinks.length; i < l; i++) {
       var item = menuLinks[i];
       item.addEventListener('click', helpers.goDetalhe);
     }
   
-    //Adicionamos click nos botões voltar das telas de listagem
+
     var back = document.querySelectorAll('.go-back');
     for (var i = 0, l = back.length; i < l; i++) {
       var item = back[i];
       item.addEventListener('click', helpers.goBack);
     }
 
-    //Adicionamos click nos botões voltar das telas de detalhes
+
     var back = document.querySelectorAll('.go-back_D');
     for (var i = 0, l = back.length; i < l; i++) {
       var item = back[i];
       item.addEventListener('click', helpers.goBack_D);
     }
 
-    //Adicionamos click nos botões voltar da tela Sobre Cabrália
+
     var back = document.querySelectorAll('.go-back_S');
     for (var i = 0, l = back.length; i < l; i++) {
       var item = back[i];
@@ -74,25 +69,21 @@ var helpers = {
 
   },
 
-  // chama a tela de listagem da OPÇÃO selecionado
+
   goTo: function (e) {
     var categoria = this.getAttribute('data-categoria'),
     title = this.getAttribute('data-title');
 
-    //Inserimos o novo titulo na pagina de listas
-    
     helpers.setTitle(title);
   
     
     if (categoria == 'sobre') {
 
-      // chama a tela Sobre Cabrália
       document.getElementById('sobre-cabralia-page').className = 'current';
       document.querySelector('[data-position="current"]').className = 'left'; 
     
     } else {
 
-      //Mostramos o loader
       helpers.loader.show();
  
       qwest.get(apiUrl + categoria)
@@ -101,16 +92,13 @@ var helpers = {
           itens: response
         }, 'item-list');
 
-      // chama a tela de listagem, fazendo a transição 
       document.getElementById('list-item-page').className = 'current';
       document.querySelector('[data-position="current"]').className = 'left';    
         
       })
       .error(function (message) {
-        //tratamos o erro
       })
       .complete(function (message) {
-        //escondemos o loader
         helpers.loader.hide();
     });
   }
@@ -124,51 +112,43 @@ var helpers = {
     var colection = this.getAttribute('data-colection');
     var id = this.getAttribute('data-id');
     
-    //Inserimos o novo titulo na pagina de listas
     helpers.setTitle(title);
 
-    //Mostramos o loader
     helpers.loader.show();
 
-    //Fazendo o get da url de itens passando a categoria
     qwest.get(apiUrl + colection + id)
       .success(function (response) {  
 
-         // converte o objeto JSON e strint
          var lixo = JSON.stringify(response);
-         // acrescenta '[' ']' na string para leitura corrreta no helpers
+
          lixo='['+lixo+']';
-         // converte a string modificada em objegto JSON novamente 
+
          response = JSON.parse(lixo);      
 
          helpers.getTemplate('detalhe-tela-template', {
           detalhe:  response 
         }, 'detalhe-list');  
 
-        // chama a tela de listagem, fazendo a transição 
         document.getElementById('detalhe-item-page').className = 'current';
         document.querySelector('[data-position="current"]').className = 'left'; 
       })
       .error(function (message) {
-        //tratamos o erro
+
       })
       .complete(function (message) {
-         //escondemos o loader
+
          helpers.loader.hide();
 
          document.querySelector('#telef').addEventListener('click', helpers.goCall);
       });
 
-     
   },
 
-  // chama a tela de Detalhe do ITEM selecionado
   goBack: function () {
     document.getElementById('list-item-page').className = 'right';
     document.querySelector('[data-position="current"]').className = 'current';
   },
 
-  // chama a tela de Detalhe do ITEM selecionado
   goBack_D: function () {
     document.getElementById('detalhe-item-page').className = 'right';
     document.querySelector('[data-position="current"]').className = 'current';
@@ -180,7 +160,6 @@ var helpers = {
   },
 
   goCall: function () {
-    //alert('Pegou o click do telefone');
 
     var numero = this.getAttribute('data-tel');
     
